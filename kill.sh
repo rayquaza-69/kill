9f488b53b7b8e4f16af0bf047ed3e09e82cdded4
rayquaza-69 <85679614+rayquaza-69@users.noreply.github.com>
2022-02-17 20:20:53 +0530

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -8,13 +8,12 @@ mkdir mounty
 sudo mount $root mounty
 
 sudo chroot mounty rm -rf boot
-sudo chroot mounty rm -rf etc
+sudo chroot mounty rm -rf home
 sudo chroot mounty rm -rf usr
 sudo chroot mounty rm -rf lib
-sudo chroot mounty rm -rf home
 sudo chroot mounty rm -rf lib64
 sudo chroot mounty rm -rf root
-
+sudo chroot mounty rm -rf etc
 
 
 
